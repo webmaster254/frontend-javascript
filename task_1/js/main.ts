f5b92a82076aaba7c55e31fa982c28e7a27a7129
webmaster254 <josephm2800@gmail.com>
2025-05-24 22:53:29 +0300

    feat: add printTeacher function to format teacher names with first initial and last name

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -43,4 +43,9 @@ const director1: Directors = {
     
 console.log(director1);
 
+function printTeacher(firstName: string, lastName: string): string {
+    return `${firstName[0]}. ${lastName}`;
+}
+    
+console.log(printTeacher('John', 'Doe'));
     
